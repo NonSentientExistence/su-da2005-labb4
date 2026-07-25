da17449ebf58b72ac2bc467cfb252ff6f4583a91
NonSentientExistence <david@venngren.se>
2026-07-25 17:10:18 +0200

    Added read CSV function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from labb4!")
+# Uppgift 1: Förbättringar i strukturen
+# Refaktorerar om från en enda stor main funktion till tre mindre funktioner med tydligt syfta och "ansvarsområde" (Inläsning från CSV, beräkningar, utskrift)
+# Beräkningsfunktionen skriver ej ut data utan returnerar bara resultatet.
 
+def read_csv(filename):
+    
+    # Read data from CSV
+    # Returns a dict with batch as key and list with values
 
-if __name__ == "__main__":
-    main()
+    data = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            batch, x, y, value = line.split(',')
+            batch = batch.strip()
+
+            if batch not in data: 
+                data[batch] = []
+            data[batch].append((float(x), float(y), float(value)))
+    return data
